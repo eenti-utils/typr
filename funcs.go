ce9574edca7a88933f45c80a267a795f3eb358d0
aedlow <aedlow@bebel>
2024-01-16 18:44:17 -0500

    add Comparator type

--- funcs.go
+++ funcs.go
@@ -1,14 +1,19 @@
 package typr
 
+// returns:
+//  -  0 if e1 == e2
+//  -  1 if e1 > e2
+//  - -1 if e1 < e2
+type Comparator[V any] func(e1, e2 V) int
 
 // returns bool true, if the specified element e "qualifies", and bool false, otherwise
 type Qualifier[V any] func(e V) (r bool)
 
 // accepts an index and an element of the specified type, and return an Operation value
-type Ranger[V any] func(i int,e V) (r Op)
+type Ranger[V any] func(i int, e V) (r Op)
 
 // returns the resolved value (as the specified type), for the specified value
-type Resolver[V,R any] func(v V) (r R)
+type Resolver[V, R any] func(v V) (r R)
 
 // returns a non-nil error, if the specified element e is considered invalid, and nil, otherwise
 type Validator[V any] func(e V) (r error)

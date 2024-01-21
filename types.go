6f0fa2dc6cb1ca477b6610471396ada792dbfb8f
aedlow <aedlow@bebel>
2024-01-20 22:39:08 -0500

    Add Value() method to Pair and Trio

--- types.go
+++ types.go
@@ -6,9 +6,21 @@ type Pair[X,Y any] struct {
 	Y Y
 }
 
+// returns a copy of the the Pair
+func (p Pair[X,Y]) Value() (r Pair[X,Y]) {
+	r = Pair[X, Y]{X: p.X, Y: p.Y}
+	return
+}
+
 // an envelope holding three values, each respectively of specified type
 type Trio[X,Y,Z any] struct {
 	X X
 	Y Y
 	Z Z
+}
+
+// returns a copy of the Trio
+func (t Trio[X,Y,Z]) Value() (r Trio[X,Y,Z]) {
+	r = Trio[X, Y, Z]{X: t.X, Y: t.Y, Z: t.Z}
+	return
 }
